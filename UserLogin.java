db1cde12f6c9fdacc4d64da44850641d8a6bd9cd
adel <m.q.b2060@gmail.com>
2024-01-09 13:01:27 +0300

    added Login in UserLogin.java

---
+++ UserLogin.java
@@ -0,0 +1,18 @@
+
+package com.learnspring.learnspring;  
+
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.RestController;  
+
+@RestController 
+
+public class HelloWorldController {      
+
+	@GetMapping("/hello")     
+
+	public String showHelloWorldController() {         
+
+	
+		
+		
+		return "hello spring boot";     } }
